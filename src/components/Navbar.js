99eee5c6aca82b1b066e212dca0b5a4480db381e
Dimitri PL <dimitrimachines@gmail.com>
2020-05-14 22:57:00 -0400

    initial pages

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+const NavBar = () => {
+  return (
+    <div className="nav-bar">
+      <h1>NAVBAR</h1>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import News from "./news";
+
+class Home extends Component {
+  render() {
+    return <div className="home">Home</div>;
+  }
+}
+
+export default Home;
