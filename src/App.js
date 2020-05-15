bd9e28ab5eed30461d6fbe0dd7e71fcfcc6c03d0
Dimitri PL <dimitrimachines@gmail.com>
2020-05-14 23:00:45 -0400

    Acorn update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+//Pages
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import NavBar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Home />
     </div>
   );
 }
