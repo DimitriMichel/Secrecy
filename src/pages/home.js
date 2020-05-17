9fb02057fca204b4de01d618fff92a6f96ed154a
Dimitri PL <dimitrimachines@gmail.com>
2020-05-16 23:46:07 -0400

    UI

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 import "../App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Tab, Tabs, TabList, TabPanel, TabPanels } from "@chakra-ui/core";
 import News from "./news";
 
 class Home extends Component {
   render() {
-    return <div className="home">Home</div>;
+    return (
+      <Tabs >
+        <TabList>
+          <Tab>One</Tab>
+          <Tab>Two</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <p>one!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    );
   }
 }
 
